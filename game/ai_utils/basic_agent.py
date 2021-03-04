c6bfbc48defeed1937cd9cc5a6f23aef85ce9bf7
Alay Shah <alayshah9@icloud.com>
2021-03-04 01:28:18 -0500

    [ADD] better functionality, still some issues with flagging

--- game/ai_utils/basic_agent.py
+++ game/ai_utils/basic_agent.py
@@ -5,20 +5,20 @@ from ..core.constants import GAME_STATE, EVENT_MOVE_UP, EVENT_MOVE_DOWN, EVENT_M
 from ..board_utils.board import Board
 from ..core.agent import Agent
 import time
+import random
+from ..board_utils.board_tile import BoardTile
 
 """
 Note: agent should not be interacted with, instead use game events
 """
+
 
 def start(board: Board, agent: Agent):
     """
     Start the basic agent
     """
 
-
-   
-
-    agent_start = (agent.i, agent.j)
+    start_tile = board.get_tile(agent.i, agent.j) #first tile we look at is the agent's starting position
     
     # is the agent finished traversing (i.e. no more moves left)
     agent_done = False
@@ -26,75 +26,124 @@ def start(board: Board, agent: Agent):
     # number of tiles flagged
     score = 0
     
-    # start at agent start
-    next_tiles = [agent_start]
+    #stores safe tiles that we want to open (holds tile objects)
+    tiles_to_open = [start_tile] # start at the agent starting position
+    
+    #store visited tiles that we are not done with, tiles who still has unflagged/unopened neighbors (holds tile objects)
+    unfinished_tiles = []
 
     while(not agent_done):
-        time.sleep(3)
+        time.sleep(1)
         pygame.event.post(pygame.event.Event(pygame.USEREVENT, attr1="force rerender"))
-        #if len(next_tiles) == 0:
-         #   next_tiles.append(agent_start)
-        
-        # pop tile off queue
-        i,j = next_tiles.pop(0)
+
+
+        if not tiles_to_open: #if the list to open new tiles is empty, then we must choose a new tile to get more information
+            random_tile = random_tile_to_open(board)
+            if not random_tile:
+                print("GAME OVER, SCORE = ", score)
+                break
+            tiles_to_open.append(random_tile)
         
         #DEBUG
-        
+        curr_tile = tiles_to_open.pop(0)
+
+        i,j = curr_tile.i, curr_tile.j
+
         # update agent position
         agent.set_pos(i,j)
-        print("agent postion: " , str(agent.i), str(agent.j))
+        #print("agent postion: " , str(agent.i), str(agent.j))
 
         # check if tile has been opened
-        curr_tile = agent.get_tile()
-        print("tile position", str(curr_tile._i), str(curr_tile._j))
-        print(curr_tile.type)
+        # print("tile position: ", str(curr_tile._i), str(curr_tile._j))
+        # print(curr_tile.type)
 
         # if the tile is unopened, we know (besides the very first) that it is safe
         if curr_tile.is_opened == False:
-            print("Opening tile")
-            agent.open_tile()
+            # print("Opening tile")
+            board.open_tile(i,j)
             # we have to reassign curr_tile since the status has changed
             curr_tile = agent.get_tile()
         
         # now, check if we accidentally opened mine
         if curr_tile.type == TILES.MINE:
-            curr_tile.toggle_flag()
+            # curr_tile.toggle_flag()
             continue
         
-        # go over all neighboring cells
-        neighbors = board.get_neighboring_tiles(i,j)
-        unopened_neighbors = [tile for tile in neighbors if tile.type == TILES.UNOPENED and not tile.is_flagged]
+        score = check_neighbors(curr_tile, board, unfinished_tiles, tiles_to_open, score)
+
+        for i in range(len(unfinished_tiles)): #since we add elements back into the queue, we only want to itnerate a specific amount of times
+            tile = unfinished_tiles.pop(0) # we want to remove the top element from the queue
+            score = check_neighbors(tile, board, unfinished_tiles, tiles_to_open, score)
+
         
-        total_neighbors = len(neighbors)
-        total_unopened_neighbors = len(unopened_neighbors)
         
-        print("total_neighbors = " + str(total_neighbors))
-        print("total_unopened_neighbors = " + str(total_unopened_neighbors))
+        
 
-        # value of the current cell (0-8)
-        clue = curr_tile.type.value
+        
 
-        #something wrong here
 
-        if clue == total_unopened_neighbors:
-            # all unopened neighbors are mines, so flag them (and add to our score :) )
-            for tile in unopened_neighbors:
+
+        
+        
+def random_tile_to_open(board: Board)-> BoardTile: #something wrong w this function
+    """
+    Pick a random tile to restart at by looking at board and choosing which 
+    """
+    available_tiles = []
+    for tilelist in board.tiles:
+        for tile in tilelist:
+             if not tile.is_opened and not tile.is_flagged:
+                 available_tiles.append(tile)
+    if not available_tiles:
+        return
+    # print(len(available_tiles))
+    print("OPENING RANDOM TILE")
+    rand = random.randint(0,len(available_tiles)-1)
+    random_tile = available_tiles[rand]
+
+    return random_tile
+
+
+
+
+
+def check_neighbors(curr_tile: BoardTile, board: Board, unfinished_tiles: list, tiles_to_open: list, score: int):
+    i, j = curr_tile.i, curr_tile.j
+    # go over all neighboring cells
+    neighbors = board.get_neighboring_tiles(i, j)
+    unopened_neighbors = [ tile for tile in neighbors if not tile.is_opened and not tile.is_flagged]
+    mine_flagged_neighbors = [tile for tile in neighbors if tile.is_flagged or (tile.is_opened and tile.type == TILES.MINE)]
+
+    total_neighbors = len(neighbors)
+    total_unopened_neighbors = len(unopened_neighbors)
+    total_mine_flagged_neighbors = len(mine_flagged_neighbors)
+
+    # print("total_neighbors = " + str(total_neighbors))
+    # print("total_unopened_neighbors = " + str(total_unopened_neighbors))
+    # print("total_mine_flagged_nieghbors = " + str(total_mine_flagged_neighbors))
+
+    clue = curr_tile.type.value
+
+
+    """
+    Still seems to be an issue somewhere here lmk if you guys see anything but over all the rest of this seems fine
+    """
+
+    # if we found all mines that are neighbors then the rest of the unopened neighbors are safe
+    if total_mine_flagged_neighbors == clue:
+        for tile in unopened_neighbors:
+            if tile not in tiles_to_open:
+                tiles_to_open.append(tile)
+    # if the number of unopened neighbors equals the clue then all neighbors are mines
+    elif total_unopened_neighbors == clue:
+        for tile in unopened_neighbors:
+            if not tile.is_flagged:
                 tile.toggle_flag()
-                score += 1
-        elif total_neighbors - clue >= total_unopened_neighbors:
-            # add all unopened neighbors into the queue
-            for tile in neighbors:
-                next_tiles.append((tile.i, tile.j))
-        elif total_unopened_neighbors == 0:
-            # no more information to get from this tile
-            continue
-        else:
-            # we add it back because there might be some information we are missing
-            next_tiles.append((i,j))
-
-        
-        
-
-
-
+                print("FLAGGED TILE")
+                print(tile.i, tile.j)
+                score+=1
+                print("Score = ", score)
+    elif total_unopened_neighbors != 0:  # if the current tile still has unopened tiles, then we are not done with it
+        unfinished_tiles.append(curr_tile)
+    return score
 
